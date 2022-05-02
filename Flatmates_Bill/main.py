1f49f8c5e9387cdf699d35b95c769fb1e0fdb95b
ivanbahutski <ivanbogutski1995@gmail.com>
2022-05-02 20:19:10 +0200

    Added the ability to generate a PDF file

--- Flatmates_Bill/main.py
+++ Flatmates_Bill/main.py
@@ -1,4 +1,6 @@
+import datetime
 from calendar import monthrange
+from fpdf import FPDF
 
 
 class Bill:
@@ -10,6 +12,16 @@ class Bill:
     def __init__(self, amount, period):
         self.amount = amount
         self.period = period
+
+    def name_month(self):
+        month_num = self.period[1]
+        month_object = datetime.datetime.strptime(str(month_num), "%m")
+        full_month_name = month_object.strftime("%B")
+        return full_month_name
+
+    def year(self):
+        year_str = str(self.period[0])
+        return year_str
 
 
 class Flatmates:
@@ -23,16 +35,10 @@ class Flatmates:
         self.day_in_house = days_in_house
 
     def pays(self, bill):
-        if type(self.day_in_house) == int:
-            num_days = monthrange(bill.period[0], bill.period[1])[1]
-            one_day = bill.amount / num_days
-            pays = self.day_in_house * one_day
-            return pays
-
-        else:
-            kf1 = self.day_in_house[0] / (sum([d for d in self.day_in_house]))
-            kf2 = self.day_in_house[1] / (sum([d for d in self.day_in_house]))
-            return bill.amount * kf1, bill.amount * kf2
+        qty_days = monthrange(bill.period[0], bill.period[1])[1]
+        one_day = bill.amount / qty_days
+        pays = self.day_in_house * one_day
+        return str(round(pays, 2))
 
 
 class PdfReport:
@@ -45,12 +51,38 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add icon
+        pdf.image(name='house.jpg', w=30, h=30)
+
+        # Insert Title
+        pdf.set_font(family="Times", size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family="Times", size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period: ", border=0)
+        pdf.cell(w=70, h=40, txt=bill.name_month(), border=0)
+        pdf.cell(w=70, h=40, txt=bill.year(), border=0, ln=1)
+        # Insert name and due amount of the first flatmate
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=70, h=25, txt=flatmate1.pays(bill), border=0, ln=1)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=70, h=25, txt=flatmate2.pays(bill), border=0, ln=1)
+
+        pdf.output(self.filename)
 
 
 the_bill = Bill(amount=120, period=(2019, 2))
-# fl = Flatmates(name=["John", "Marry"], days_in_house=[20, 25])
-fl = Flatmates(name="Ivan", days_in_house=20)
 
-print(fl.pays(bill=the_bill))
-# print(marry.pays(bill=the_bill))
+fl1 = Flatmates(name="Ivan", days_in_house=20)
+fl2 = Flatmates(name='Vlad', days_in_house=25)
+
+pdf_report = PdfReport('bill.pdf')
+pdf_report.generate(flatmate1=fl1, flatmate2=fl2, bill=the_bill)
+
